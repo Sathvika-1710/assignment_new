bec6569c689363ceaf690208a805cc73a9e15376
Sathvika-1710 <106346564+Sathvika-1710@users.noreply.github.com>
2022-06-26 20:07:10 +0530

    Add files via upload

---
+++ src/components/Index.js
@@ -0,0 +1,392 @@
+import React from 'react'
+import '../components/index.css'
+
+
+export const Index = () => {
+    return (
+        <>
+            <div className='container'>
+                <br />
+                <div className='buttons'>
+
+                    <button type="button" class="btn btn-dark btn-sm mg-3">Login</button>
+                    <button type="button" class="btn btn-light btn-sm">Get Started</button>
+
+                </div>
+                <h3 className='heading'>Your gateway to </h3>
+                <h3 className='heading'>Product Industry</h3>
+                <br />
+                <div className='sub_heading'>
+                    <div>
+                        <div className='para'>
+                            <p>A new kind of internet university-one that is focused on helping you build a newtork,learn new skills and build the right mindset</p>
+                        </div>
+                        <button type="button" class="btn btn-dark btn-sm mg-3">Deep down into Courses</button>
+                    </div>
+
+                </div>
+                <br />
+                <br />
+                <br />
+                <h3 className='heading' id='main'>Here is what you'll learn and become!</h3>
+
+                <br />
+                <div className='items'>
+                    <div className='sub_item'>
+                        <div className='box'>
+
+                        </div>
+                        <h4 className='sub_heading2' >Product Design</h4>
+                        <div className='sub_para'>
+                            <p >You’ll learn from the best in the design industry; join Dan Mall and the former hiring manager at Google, for live and interactive sessions.</p>
+                        </div>
+                        <br />
+
+                        <div className='sub_buttons'>
+                            <button type="button" class="btn btn-light btn-sm">Insight</button>
+
+                        </div>
+
+                    </div>
+
+                    <div className='sub_item'>
+                        <div className='box'>
+
+                        </div>
+                        <h4 className='sub_heading2' >Product Management</h4>
+                        <div className='sub_para'>
+                            <p >You’ll learn from the best in the design industry; join Dan Mall and the former hiring manager at Google, for live and interactive sessions.</p>
+                        </div>
+                        <br />
+
+                        <div className='sub_buttons'>
+                            <button type="button" class="btn btn-light btn-sm">Insight</button>
+
+                        </div>
+
+                    </div>
+
+                    <div className='sub_item'>
+                        <div className='box'>
+
+                        </div>
+                        <h4 className='sub_heading2' >Product Growth</h4>
+                        <div className='sub_para'>
+                            <p >You’ll learn from the best in the design industry; join Dan Mall and the former hiring manager at Google, for live and interactive sessions.</p>
+                        </div>
+                        <br />
+
+                        <div className='sub_buttons'>
+                            <button type="button" class="btn btn-light btn-sm">Insight</button>
+
+                        </div>
+
+                    </div>
+                </div>
+                <br />
+                <br />
+                <h5>CURRICULUM</h5>
+                <h3 className='heading'>Learn-by-doing with our extensive 12-week product design course</h3>
+                <br />
+                <br />
+                <div className='items2'>
+                    <div className='sub_item1'>
+                        <div className='small_box'>
+                            <p className="num">1</p>
+                        </div>
+                        <h2 >Start your course</h2>
+                        <br />
+                        <div className='sub-para'>
+                            <p>Introduce yourself in the student Slack, meet your mentor, and get set-up by reviewing course material.</p>
+                        </div>
+                    </div>
+
+                    <div className='sub_item1'>
+                        <div className='small_box'>
+                            <p className="num">2</p>
+                        </div>
+                        <h2 className='sub_heading2'>Start your course</h2>
+                        <br />
+                        <div className='sub-para'>
+                            <p>Introduce yourself in the student Slack, meet your mentor, and get set-up by reviewing course material.</p>
+                        </div>
+                    </div>
+
+                    <div className='sub_item1'>
+                        <div className='small_box'>
+                            <p className="num">3</p>
+                        </div>
+                        <h2 >Start your course</h2>
+                        <br />
+                        <div className='sub-para'>
+                            <p>Introduce yourself in the student Slack, meet your mentor, and get set-up by reviewing course material.</p>
+                        </div>
+                    </div>
+
+                    <div className='sub_item1'>
+                        <div className='small_box'>
+                            <p className="num">4</p>
+                        </div>
+                        <h2 className='sub_heading2'>Start your course</h2>
+                        <br />
+                        <div className='sub-para'>
+                            <p>Introduce yourself in the student Slack, meet your mentor, and get set-up by reviewing course material.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <br />
+                <br />
+                <br />
+                <div className='items'>
+                    <div className='sub_item3'>
+                        <h5>Week One</h5>
+                        <br />
+
+                        <h3>Getting Started</h3>
+                        <br />
+                        <div className='sub_para'>
+                            <ul>
+                                <li><p>Breaking the ice and meeting your classmates</p></li>
+                                <li><p>Getting to know your instructor</p></li>
+                                <li><p>Introduction to UX and UI design</p></li>
+                                <li><p>What is design thinking process</p></li>
+                                <li><p>The double diamond approach</p></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className='sub_item3'>
+                        <h5>Week One</h5>
+                        <br />
+
+                        <h3>Getting Started</h3>
+                        <br />
+                        <div className='sub_para'>
+                            <ul>
+                                <li><p>Breaking the ice and meeting your classmates</p></li>
+                                <li><p>Getting to know your instructor</p></li>
+                                <li><p>Introduction to UX and UI design</p></li>
+                                <li><p>What is design thinking process</p></li>
+                                <li><p>The double diamond approach</p></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className='sub_item3'>
+                        <h5>Week One</h5>
+                        <br />
+
+                        <h3>Getting Started</h3>
+                        <br />
+
+                        <div className='sub_para'>
+                            <ul>
+                                <li><p>Breaking the ice and meeting your classmates</p></li>
+                                <li><p>Getting to know your instructor</p></li>
+                                <li><p>Introduction to UX and UI design</p></li>
+                                <li><p>What is design thinking process</p></li>
+                                <li><p>The double diamond approach</p></li>
+                            </ul>
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <br />
+                <br />
+
+                <div className='items'>
+                    <div className='sub_item3'>
+                        <h5>Week One</h5>
+                        <br />
+
+                        <h3>Getting Started</h3>
+                        <br />
+                        <div className='sub_para'>
+                            <ul>
+                                <li><p>Breaking the ice and meeting your classmates</p></li>
+                                <li><p>Getting to know your instructor</p></li>
+                                <li><p>Introduction to UX and UI design</p></li>
+                                <li><p>What is design thinking process</p></li>
+                                <li><p>The double diamond approach</p></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className='sub_item3'>
+                        <h5>Week One</h5>
+                        <br />
+
+                        <h3>Getting Started</h3>
+                        <br />
+                        <div className='sub_para'>
+                            <ul>
+                                <li><p>Breaking the ice and meeting your classmates</p></li>
+                                <li><p>Getting to know your instructor</p></li>
+                                <li><p>Introduction to UX and UI design</p></li>
+                                <li><p>What is design thinking process</p></li>
+                                <li><p>The double diamond approach</p></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className='sub_item3'>
+                        <h5>Week One</h5>
+                        <br />
+
+                        <h3>Getting Started</h3>
+                        <br />
+
+                        <div className='sub_para'>
+                            <ul>
+                                <li><p>Breaking the ice and meeting your classmates</p></li>
+                                <li><p>Getting to know your instructor</p></li>
+                                <li><p>Introduction to UX and UI design</p></li>
+                                <li><p>What is design thinking process</p></li>
+                                <li><p>The double diamond approach</p></li>
+                            </ul>
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <br />
+                <button type="button" class="btn btn-dark btn-la" className='button_large'><p className='para_inner3'>Expand All</p></button>
+                <br />
+                <br />
+                <br />
+                <h5>CATCH</h5>
+                <h3 className='heading'>One week In-house sponsered design camp in mountains</h3>
+                <br />
+                <br />
+                <div className='items'>
+                    <div className='large_box'>
+
+                    </div>
+                    <div className='large_box'>
+
+                    </div>
+                    <div className='large_box'>
+
+                    </div>
+                    <div className='large_box'>
+
+                    </div>
+                </div>
+                <br />
+                <br />
+                <div className='items'>
+                    <div className='large_box'>
+
+                    </div>
+                    <div className='large_box'>
+
+                    </div>
+                    <div className='large_box'>
+
+                    </div>
+                    <div className='large_box'>
+
+                    </div>
+                </div>
+
+                <br />
+                <br />
+                <div className='footer'>
+                    <br />
+                    <br />
+                    <br />
+                    <h4>WHAT'S INCLUDED</h4>
+                    <h3 className='heading'>An Immersive and interactive learning experience</h3>
+                    <br/>
+                    <br/>
+                    <div className='items' id="footer">
+                        <div className='footer_item'>
+                            <div className='small_box2'>
+
+                            </div>
+                            <h3>Product Design</h3>
+                            <div className='sub_para'>
+                                <p>You’ll learn from the best in the design industry; join Dan Mall and the former hiring manager at Google, for live and interactive sessions.</p>
+                            </div>
+                        </div>
+
+                        <div className='footer_item'>
+                            <div className='small_box2'>
+
+                            </div>
+                            <h3>Product Design</h3>
+                            <div className='sub_para'>
+                                <p>You’ll learn from the best in the design industry; join Dan Mall and the former hiring manager at Google, for live and interactive sessions.</p>
+                            </div>
+                        </div>
+
+                        <div className='footer_item'>
+                            <div className='small_box2'>
+
+                            </div>
+                            <h3>Product Design</h3>
+                            <div className='sub_para'>
+                                <p>You’ll learn from the best in the design industry; join Dan Mall and the former hiring manager at Google, for live and interactive sessions.</p>
+                            </div>
+                        </div>
+
+
+
+                        
+                    </div>
+
+                    <br/>
+                    <br/>
+
+                    <div className='items' id="footer">
+                        <div className='footer_item'>
+                            <div className='small_box2'>
+
+                            </div>
+                            <h3>Product Design</h3>
+                            <div className='sub_para'>
+                                <p>You’ll learn from the best in the design industry; join Dan Mall and the former hiring manager at Google, for live and interactive sessions.</p>
+                            </div>
+                        </div>
+
+                        <div className='footer_item'>
+                            <div className='small_box2'>
+
+                            </div>
+                            <h3>Product Design</h3>
+                            <div className='sub_para'>
+                                <p>You’ll learn from the best in the design industry; join Dan Mall and the former hiring manager at Google, for live and interactive sessions.</p>
+                            </div>
+                        </div>
+
+                        <div className='footer_item'>
+                            <div className='small_box2'>
+
+                            </div>
+                            <h3>Product Design</h3>
+                            <div className='sub_para'>
+                                <p>You’ll learn from the best in the design industry; join Dan Mall and the former hiring manager at Google, for live and interactive sessions.</p>
+                            </div>
+                        </div>
+
+
+
+                        
+                    </div>
+
+
+
+                </div>
+
+
+            </div>
+
+
+
+        </>
+    )
+}
